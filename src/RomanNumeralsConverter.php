a463d151ce80a934e7b2b88b32b3436924217b8f
Cora Marcu <coramarcu@gmail.com>
2024-06-10 21:08:22 +0100

    pass testThatItConverts49toXLIX()

--- src/RomanNumeralsConverter.php
+++ src/RomanNumeralsConverter.php
@@ -2,41 +2,31 @@
 
 final class RomanNumeralsConverter {
     public function convertToRoman(int $number): string {
-        switch (true) {
-            case ($number == 1):
-                return "I";
-            case ($number >= 1 && $number < 4): {
-                $roman_numeral = "";
+        $roman_symbols = [
+            'M'  => 1000,
+            'CM' => 900,
+            'D'  => 500,
+            'CD' => 400,
+            'C'  => 100,
+            'XC' => 90,
+            'L'  => 50,
+            'XL' => 40,
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1
+        ];
 
-                for($i = $number; $i >= 1; $i--) {
-                    $roman_numeral .= "I";
-                }
-                return $roman_numeral;
+        $roman_numeral = "";
+
+        foreach ($roman_symbols as $symbol => $value) {
+            while ($number >= $value) {
+                $roman_numeral .= $symbol;
+                $number -= $value;
             }
-            case ($number == 4):
-                return "IV";
-            case ($number == 5):
-                return "V";
-            case ($number == 9):
-                return "IX";
-            case ($number == 10):
-                return "X";
-            case ($number == 40):
-                return "XL";
-            case ($number == 50): 
-                return "L";
-            case ($number == 90):
-                return "XC";
-            case ($number == 100):
-                return "C";
-            case ($number == 400):
-                return "CD";
-            case ($number == 500):
-                return "D";
-            case ($number == 900):
-                return "CM";
-            case ($number == 1000):
-                return "M";
         }
+
+        return $roman_numeral;
     }
 }
