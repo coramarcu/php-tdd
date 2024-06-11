7bc9385d734bfe763823fc27123ce1789e3c0905
Cora Marcu <coramarcu@gmail.com>
2024-06-11 10:40:06 +0100

    add a setUp() function  to keep code dry

--- tests/RomanNumeralsConverterTest.php
+++ tests/RomanNumeralsConverterTest.php
@@ -1,100 +1,91 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+require_once 'src/RomanNumeralsConverter.php';
 
 final class RomanNumeralsConverterTest extends TestCase
 {
+    protected RomanNumeralsConverter $converter;
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->converter = new RomanNumeralsConverter();
+    }
     public function testThat1convertsToI(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(1);
+        $result = $this->converter->convertToRoman(1);
         $this->assertSame('I', $result);
     }
     public function testThat5ConvertsToV(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(5);
+        $result = $this->converter->convertToRoman(5);
         $this->assertSame('V', $result);
     }
 
     public function testThat10ConvertsToX(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(10);
+        $result = $this->converter->convertToRoman(10);
         $this->assertSame('X', $result);
     }
 
     public function testThat50ConvertsToL(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(50);
+        $result = $this->converter->convertToRoman(50);
         $this->assertSame('L', $result);
     }
 
     public function testThat100ConvertsToC(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(100);
+        $result = $this->converter->convertToRoman(100);
         $this->assertSame('C', $result);
     }
 
     public function testThat500ConvertsToD(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(500);
+        $result = $this->converter->convertToRoman(500);
         $this->assertSame('D', $result);
     }
 
     public function testThat1000ConvertsToM(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(1000);
+        $result = $this->converter->convertToRoman(1000);
         $this->assertSame('M', $result);
     }
 
     public function testThat4ConvertsToIV(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(4);
+        $result = $this->converter->convertToRoman(4);
         $this->assertSame('IV', $result);
     }
 
     public function testThat9ConvertsToIX(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(9);
+        $result = $this->converter->convertToRoman(9);
         $this->assertSame('IX', $result);
     }
 
     public function testThat40ConvertsToXL(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(40);
+        $result = $this->converter->convertToRoman(40);
         $this->assertSame('XL', $result);
     }
 
     public function testThat90ConvertsToXC(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(90);
+        $result = $this->converter->convertToRoman(90);
         $this->assertSame('XC', $result);
     }
 
     public function testThat400ConvertsToCD(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(400);
+        $result = $this->converter->convertToRoman(400);
         $this->assertSame('CD', $result);
     }
 
     public function testThat900ConvertsToCM(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(900);
+        $result = $this->converter->convertToRoman(900);
         $this->assertSame('CM', $result);
     }
 
     public function testThatItWorksWithNumbersBetween1and4(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(3);
+        $result = $this->converter->convertToRoman(3);
         $this->assertSame('III', $result);
     }
 
     public function testThatItConverts49toXLIX(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(49);
+        $result = $this->converter->convertToRoman(49);
         $this->assertSame('XLIX', $result);
     }
 
     public function testThatItConverts1999toMCMXCIX(): void {
-        $converter = new RomanNumeralsConverter();
-        $result = $converter->convertToRoman(1999);
+        $result = $this->converter->convertToRoman(1999);
         $this->assertSame('MCMXCIX', $result);
     }
 
